03da2bd167a3eb158bc9771a7d65a6073b4ba9bb
Abdullah Uppal <au00152@gmail.com>
2026-05-14 05:53:33 -0700

    Redis initial setup P1

---
+++ storeapi/cache_config.py
@@ -0,0 +1,26 @@
+import redis
+
+
+class Cache:
+
+    @staticmethod
+    def __init__(self):
+         self.r = redis.Redis(host='localhost', port=6379, decode_responses=True)
+
+    @staticmethod
+    def close_cache(self):
+        self.r
+
+    def get_response(self, key:str): 
+        res = self.r.get(key)
+        return res 
+
+    def set_response(self, key:str, response:dict):
+        self.r.hset(key, mapping=response, ex=600)
+
+
+    
+    
+    
+
+    
--- storeapi/main.py
+++ storeapi/main.py
@@ -8,6 +8,7 @@ from fastapi.exception_handlers import http_exception_handler
 
 from database import database
 from logging_config import configure_logging
+from cache_config import Cache
 from routers.posts import router as posts_router
 from routers.upload import router as upload_router
 from routers.user import router as user_router
@@ -19,8 +20,10 @@ logger = logging.getLogger(__name__)
 async def lifespan(app: FastAPI):
     configure_logging()  # making sure the log is configured before the app starts
     logger.info("Hello World")
+    Cache.configure_cache()
     await database.connect()  # run this when app is ready to run
     yield  # wait
+    Cache.close_cache()
     await database.disconnect()  # run this when app is shutting down
 
 
@@ -32,7 +35,6 @@ app.add_middleware(CorrelationIdMiddleware)  # add the middleware to the app
 app.include_router(posts_router)
 app.include_router(upload_router)
 app.include_router(user_router)
-
 
 # adding the log and raising proper error msg if we get an error
 @app.exception_handler(HTTPException)
